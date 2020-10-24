808f6953caa7a64adfddf02a79d8f56a90adbfc2
ysbing <ysbing@ysbing.com>
2020-10-24 15:56:09 +0800

    Initial commit

---
+++ app/src/main/java/com/ysbing/yadb/Device.java
@@ -0,0 +1,73 @@
+package com.ysbing.yadb;
+
+import android.os.SystemClock;
+import android.view.InputDevice;
+import android.view.InputEvent;
+import android.view.KeyCharacterMap;
+import android.view.KeyEvent;
+
+import com.ysbing.yadb.wrappers.ClipboardManager;
+import com.ysbing.yadb.wrappers.InputManager;
+import com.ysbing.yadb.wrappers.ServiceManager;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+
+public final class Device {
+
+    private final ServiceManager serviceManager = new ServiceManager();
+
+    private final AtomicBoolean isSettingClipboard = new AtomicBoolean();
+
+    public boolean injectEvent(InputEvent inputEvent, int mode) {
+        return serviceManager.getInputManager().injectInputEvent(inputEvent, mode);
+    }
+
+    public boolean injectEvent(InputEvent event) {
+        return injectEvent(event, InputManager.INJECT_INPUT_EVENT_MODE_ASYNC);
+    }
+
+    public boolean injectKeyEvent(int action, int keyCode, int repeat, int metaState) {
+        long now = SystemClock.uptimeMillis();
+        KeyEvent event = new KeyEvent(now, now, action, keyCode, repeat, metaState, KeyCharacterMap.VIRTUAL_KEYBOARD, 0, 0,
+                InputDevice.SOURCE_KEYBOARD);
+        return injectEvent(event);
+    }
+
+    public boolean injectKeycode(int keyCode) {
+        return injectKeyEvent(KeyEvent.ACTION_DOWN, keyCode, 0, 0) && injectKeyEvent(KeyEvent.ACTION_UP, keyCode, 0, 0);
+    }
+
+    public String getClipboardText() {
+        ClipboardManager clipboardManager = serviceManager.getClipboardManager();
+        if (clipboardManager == null) {
+            return null;
+        }
+        CharSequence s = clipboardManager.getText();
+        if (s == null) {
+            return null;
+        }
+        return s.toString();
+    }
+
+    public boolean setClipboardText(String text) {
+        ClipboardManager clipboardManager = serviceManager.getClipboardManager();
+        if (clipboardManager == null) {
+            return false;
+        }
+
+        String currentClipboard = getClipboardText();
+        if (currentClipboard != null && currentClipboard.equals(text)) {
+            // The clipboard already contains the requested text.
+            // Since pasting text from the computer involves setting the device clipboard, it could be set twice on a copy-paste. This would cause
+            // the clipboard listeners to be notified twice, and that would flood the Android keyboard clipboard history. To workaround this
+            // problem, do not explicitly set the clipboard text if it already contains the expected content.
+            return false;
+        }
+
+        isSettingClipboard.set(true);
+        boolean ok = clipboardManager.setText(text);
+        isSettingClipboard.set(false);
+        return ok;
+    }
+
+}
---
+++ app/src/main/java/com/ysbing/yadb/wrappers/ServiceManager.java
@@ -0,0 +1,58 @@
+package com.ysbing.yadb.wrappers;
+
+import android.annotation.SuppressLint;
+import android.os.IBinder;
+import android.os.IInterface;
+
+import java.lang.reflect.Method;
+
+@SuppressLint("PrivateApi,DiscouragedPrivateApi")
+public final class ServiceManager {
+
+    public static final String PACKAGE_NAME = "com.android.shell";
+    public static final int USER_ID = 0;
+
+    private final Method getServiceMethod;
+
+    private ClipboardManager clipboardManager;
+    private InputManager inputManager;
+
+    public ServiceManager() {
+        try {
+            getServiceMethod = Class.forName("android.os.ServiceManager").getDeclaredMethod("getService", String.class);
+        } catch (Exception e) {
+            throw new AssertionError(e);
+        }
+    }
+
+    private IInterface getService(String service, String type) {
+        try {
+            IBinder binder = (IBinder) getServiceMethod.invoke(null, service);
+            Method asInterfaceMethod = Class.forName(type + "$Stub").getMethod("asInterface", IBinder.class);
+            return (IInterface) asInterfaceMethod.invoke(null, binder);
+        } catch (Exception e) {
+            throw new AssertionError(e);
+        }
+    }
+
+    public ClipboardManager getClipboardManager() {
+        if (clipboardManager == null) {
+            IInterface clipboard = getService("clipboard", "android.content.IClipboard");
+            if (clipboard == null) {
+                // Some devices have no clipboard manager
+                // <https://github.com/Genymobile/scrcpy/issues/1440>
+                // <https://github.com/Genymobile/scrcpy/issues/1556>
+                return null;
+            }
+            clipboardManager = new ClipboardManager(clipboard);
+        }
+        return clipboardManager;
+    }
+
+    public InputManager getInputManager() {
+        if (inputManager == null) {
+            inputManager = new InputManager(getService("input", "android.hardware.input.IInputManager"));
+        }
+        return inputManager;
+    }
+}
